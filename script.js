ded59e1edfdc6660363d769390bf4d348bf31752
jahighshaw-tech <jahighshaw@uncg.edu>
2026-04-09 23:14:26 +0000

    Added clints

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ button.addEventListener('click', clickThisButton)
 function clickThisButton() {
     alert('I did it!')
 }
+
+const button2 = document.getElementById("myButton");
+
+button2.addEventListener("click", function() {
+    alert('I did it again!')
+    button2.style.fontSize = '30px';
+    document.body.style.backgroundColor = 'lightblue';
+});
+
