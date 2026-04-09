3c720d7d637ddea44a4d295b7f2e1fd4a690c115
jahighshaw-tech <jahighshaw@uncg.edu>
2026-04-09 23:18:05 +0000

    fixed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const button = document.getElementById('thisButton')
 button.addEventListener('click', clickThisButton)
 
 function clickThisButton() {
-    alert('I did it!')
+    alert('I did it')
 }
 
 const button2 = document.getElementById("myButton");
